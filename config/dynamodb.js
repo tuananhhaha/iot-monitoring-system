c275c124b5b1d3e8d90a4962e2136dc875e53a5d
tuananhhaha <128787892+tuananhhaha@users.noreply.github.com>
2026-09-13 10:49:07 +0700

    Add DynamoDB configuration

---
+++ config/dynamodb.js
@@ -0,0 +1,10 @@
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBDocumentClient } = require('@aws-sdk/lib-dynamodb');
+
+const client = new DynamoDBClient({
+  region: process.env.AWS_REGION || 'us-east-1',
+});
+
+const docClient = DynamoDBDocumentClient.from(client);
+
+module.exports = docClient;
